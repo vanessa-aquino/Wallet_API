4c3ff97af6fe94c2b08a62ecbd541d6d0f258282
Vanessa <vanessaquinoo@hotmail.com>
2025-06-17 11:06:54 -0300

    fix: deleted the Role field from the RegisterDto as it is being automated

--- Models/DTOs/User/RegisterDto.cs
+++ Models/DTOs/User/RegisterDto.cs
@@ -10,6 +10,5 @@ namespace WalletAPI.Models.DTOs.User
         public string Email { get; set; }
         public string Phone { get; set; }
         public string Password { get; set; }
-        public UserRole Role { get; set; }
     }
 }
